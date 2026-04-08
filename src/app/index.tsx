d3385df3e63b5af967f2c62cab8835c5eeb35189
KAUAN BORGES SANTOS <kauan.bsantos2@senacsp.edu.br>
2026-04-07 21:15:31 -0300

    att

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,35 @@
-import { View } from "react-native";
+import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
+import { CATEGORIES } from "@/utils/data/products";
+import { useState } from "react";
+import { FlatList, View } from "react-native";
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  function hardleCategorySelect(selectCategory: string) {
+    setCategory(selectCategory);
+  }
+
   return (
     <View className="flex-1 pt-8">
       <Header title="Faça seu pedido" cartQuantityItems={5} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => hardleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
     </View>
   );
 }
